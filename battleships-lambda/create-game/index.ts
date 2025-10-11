ab38d99a1a2fa587cdd40f883279a8de99abe88f
FlixonStudios <chiakaiming@gmail.com>
2025-10-11 17:31:21 +0800

    install s3 from sdk and use put object

--- battleships-lambda/create-game/index.ts
+++ battleships-lambda/create-game/index.ts
@@ -1,9 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { randomUUID } from "crypto";
 import { getNewBoard } from "./common/constants";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
-export const createGameHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        const s3 = new S3Client({ region: process.env.AWS_REGION }); // AWS_REGION is a reserved keyword for AWS, for now its okay to leave as is
+        const BUCKET_NAME = process.env.GAMES_BUCKET!; // set in lambda, TODO: we should inject this value
         const gameCode = generateGameCode();
         const playerId = randomUUID();
 
@@ -12,6 +15,15 @@ export const createGameHandler = async (event: APIGatewayProxyEvent): Promise<AP
             players: [{ id: playerId, ready: false, board: getNewBoard() }],
             createdAt: new Date().toISOString(),
         };
+
+        await s3.send(
+            new PutObjectCommand({
+                Bucket: BUCKET_NAME,
+                Key: `games/${gameCode}.json`,
+                Body: JSON.stringify(initialGameState),
+                ContentType: "application/json",
+            }),
+        );
 
         return {
             statusCode: 200,
