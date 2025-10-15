7704cf2a7c12dfb814f49726fba02cae45cb3bf5
FlixonStudios <chiakaiming@gmail.com>
2025-10-15 18:07:53 +0800

    linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 module.exports = {
+    entry: "./src/index.ts",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
         path: path.resolve(__dirname, "public"),
+    },
+    plugins: [new Dotenv()],
 };
