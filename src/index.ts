ba536542db376621fd9657c71d931a29ed0ca4a6
FlixonStudios <chiakaiming@gmail.com>
2025-10-20 14:35:37 +0800

    add copy game code function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createGame } from "./apis/create-game";
 import { joinGame } from "./apis/join-game";
 import { addPlayer } from "./components/add-player";
+import { enableGameCodeCopy } from "./components/enable-game-code-copy";
 import { appConfig } from "./config/app-config";
 import { FP_GAME_STATE } from "./constants";
 
@@ -8,7 +9,8 @@ const token = ""; // FIXME: get from cookies
 
 const statusEl = document.getElementById("status") as HTMLDivElement;
 const gameAreaEl = document.getElementById("gameArea") as HTMLDivElement;
-
+const joinBtn = document.getElementById("joinGameBtn") as HTMLButtonElement;
+const gameCodeEl = document.getElementById("gameCode") as HTMLSpanElement;
 const joinCodeInput = document.getElementById("joinCode") as HTMLInputElement;
 
 let playerId: string | null = null;
@@ -17,17 +19,18 @@ const createGameBtn = document.getElementById("createGameBtn") as HTMLButtonElem
 
 createGameBtn.addEventListener("click", async () => {
     const response = await createGame(token);
-
+    const gameCode = response?.code;
     if (appConfig.deployEnv === "local") {
         sessionStorage.setItem(FP_GAME_STATE, JSON.stringify(response.gameState));
     }
 
-    gameCode.innerText = response?.code || "error";
-    addPlayer(response?.playerId);
-});
+    gameCodeEl.innerText = gameCode || "error";
 
-const joinBtn = document.getElementById("joinGameBtn") as HTMLButtonElement;
-const gameCode = document.getElementById("gameCode") as HTMLSpanElement;
+    if (gameCode) {
+        addPlayer(response?.playerId);
+        enableGameCodeCopy();
+    }
+});
 
 joinBtn.addEventListener("click", async () => {
     const response = await joinGame(joinCodeInput.value);
