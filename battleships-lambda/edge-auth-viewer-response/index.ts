4023eb0137819fb618996dd197dcf42f4658ac92
FlixonStudios <chiakaiming@gmail.com>
2025-11-07 15:03:51 +0800

    add code for viewer response lambda edge

---
+++ battleships-lambda/edge-auth-viewer-response/index.ts
@@ -0,0 +1,23 @@
+// https://github.com/aws/aws-lambda-nodejs-runtime-interface-client/issues/137
+
+import { CloudFrontResponseEvent } from "aws-lambda";
+
+export const handler = async (event: CloudFrontResponseEvent) => {
+    const FP_AUTH_TOKEN = "fp-auth-token";
+
+    const response = event.Records?.[0]?.cf?.response;
+
+    let headers = response?.headers;
+
+    const authToken = headers[FP_AUTH_TOKEN]?.[0]?.value;
+
+    if (authToken) {
+        const { [FP_AUTH_TOKEN]: _removedAuthToken, ...otherHeaders } = headers;
+        response.headers = otherHeaders;
+        response.headers["set-cookie"] = [
+            { key: "Set-Cookie", value: `${FP_AUTH_TOKEN}=${authToken}; Path=/; Secure; SameSite=None;` },
+        ];
+    }
+
+    return response;
+};
