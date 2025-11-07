7f4b34e924471139c5db5bd8e392a35bd66b17e9
FlixonStudios <chiakaiming@gmail.com>
2025-11-07 15:00:09 +0800

    update code since we are not able to set cookie via origin if it passes through CF

--- battleships-lambda/create-game/index.ts
+++ battleships-lambda/create-game/index.ts
@@ -2,10 +2,24 @@ import { randomUUID } from "crypto";
 import { getNewBoard } from "./common/constants";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
+interface CreateGameResponse {
+    statusCode: number;
+    headers: {
+        "fp-auth-token": `${string}-${string}-${string}-${string}-${string}`;
+        "Access-Control-Allow-Headers": string;
+        "Access-Control-Allow-Credentials": string;
+        "Access-Control-Allow-Origin": string;
+    };
+    body: string;
+    multiValueHeaders?: Record<string, Array<string>>;
+}
+
 export const handler = async (event: any) => {
     const FP_AUTH_TOKEN = "fp-auth-token";
-    const FP_USER_ID = "fp-user-id";
+
     try {
+        // Note: event.Records?.[0].cf?.request is undefined (becos this is not Lambda Edge)
+
         const env = process.env.DEPLOY_ENV;
         const LOCAL_ENV = "local";
 
@@ -13,7 +27,7 @@ export const handler = async (event: any) => {
         const playerName = body.playerName;
 
         const s3 = new S3Client({ region: process.env.AWS_REGION }); // AWS_REGION is a reserved keyword for AWS, for now its okay to leave as is
-        const BUCKET_NAME = process.env.GAMES_BUCKET!; // set in lambda, TODO: we should inject this value
+        const BUCKET_NAME = process.env.GAMES_BUCKET!; // set in lambda, TODO: we should inject this value in pipeline
         const gameCode = generateGameCode();
         const playerId = randomUUID();
 
@@ -34,20 +48,13 @@ export const handler = async (event: any) => {
             );
         }
 
-        const cookieConfig = LOCAL_ENV ? "Path=/; SameSite=Lax" : "Path=/; Secure; SameSite=None";
-
-        return {
+        const response: CreateGameResponse = {
             statusCode: 200,
             headers: {
-                "Access-Control-Allow-Origin": "*", // FIXME: restrict origins
+                [FP_AUTH_TOKEN]: playerId,
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Credentials": "true",
-            },
-            multiValueHeaders: {
-                "Set-Cookie": [
-                    `${FP_AUTH_TOKEN}=${playerId}; ${cookieConfig}`,
-                    `${FP_USER_ID}=${playerId}; ${cookieConfig}`,
-                ],
+                "Access-Control-Allow-Origin": "*",
             },
             body: JSON.stringify({
                 gameCode,
@@ -55,6 +62,27 @@ export const handler = async (event: any) => {
                 gameState: initialGameState,
             }),
         };
+
+        if (env === LOCAL_ENV) {
+            // we set cookie for local since prd is set thru LambdaEdge
+            const cookieConfig = "Path=/; SameSite=Lax";
+            response.headers["Access-Control-Allow-Origin"] = "*";
+            response.multiValueHeaders = {
+                "Set-Cookie": [`${FP_AUTH_TOKEN}=${playerId}; ${cookieConfig}`],
+            };
+        } else {
+            /**
+            // following code is just for reference due to above
+            const domainTld = process.env.BASE_URL?.replace("https://", "").split(/\./).slice(-3).join(".");
+            // The value of domain has to be <CF-HASH>.cloudfront.net TODO: Test this
+            const domainCookieConfig = "Domain=" + domainTld;
+            const cookieConfig = `Path=/; Secure; SameSite=None; ${domainTld ? domainCookieConfig : ""}`;
+             */
+
+            response.headers["Access-Control-Allow-Origin"] = process.env.BASE_URL ?? "*";
+        }
+      
+        return response;
     } catch (err) {
         console.log(err);
         return {
