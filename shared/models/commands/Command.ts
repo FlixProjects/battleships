f390c8096f87ab2f7f36b9cda42ffcc5bc10e004
FlixonStudios <chiakaiming@gmail.com>
2026-03-17 13:16:47 +0800

    create ICommand interface

--- shared/models/commands/Command.ts
+++ shared/models/commands/Command.ts
@@ -1,6 +1,6 @@
-import { IGameStateManager } from "../../types";
+import { ICommand, ICommandExecutionParams } from "./types";
 
-export abstract class Command {
-    abstract execute(gsm?: IGameStateManager): void;
-    abstract undo(gsm?: IGameStateManager): void;
+export abstract class Command implements ICommand {
+    abstract execute(params: ICommandExecutionParams): Promise<void>;
+    abstract undo(params: ICommandExecutionParams): Promise<void>;
 }
--- shared/models/commands/types.ts
+++ shared/models/commands/types.ts
@@ -1,6 +1,19 @@
+import { IGame, IGameStateManager } from "../../types";
+
 export const CommandType = {
     Client: "Client",
     Server: "Server",
 };
 
 export type TCommandType = keyof typeof CommandType;
+
+export interface ICommandExecutionParams {
+    currentPlayerId: string;
+    gsm?: IGameStateManager;
+    game: IGame;
+}
+
+export interface ICommand {
+    execute(params: ICommandExecutionParams): Promise<void>;
+    undo(params: ICommandExecutionParams): Promise<void>;
+}
