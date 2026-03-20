56c1a002848d48ae0f3ebacffb426bb3952d1fac
FlixonStudios <chiakaiming@gmail.com>
2026-03-20 14:01:29 +0800

    separate game logic from ShipEntity

--- shared/models/Ship.ts
+++ shared/models/Ship.ts
@@ -1,60 +1,9 @@
-import { IHull, IHullTemplate, IShip, locationToKey, PathHelper } from "../../shared";
-import { Entity } from "./entities";
-import { Hull } from "./Hull";
+import { IShip, locationToKey, PathHelper } from "../../shared";
+import { ShipEntity } from "./entities/ShipEntity";
 
-export class Ship extends Entity<Ship> implements IShip {
-    id: string;
-    playerId: string;
-    refNo: string;
-    name: string;
-    hullIds?: string[];
-    hulls?: Hull[];
-    dimensions: [number, number];
-    deployed: boolean;
-    commandPointCost: number;
-    movementRange: number;
-    remainingMovement: number;
-    movementCommandPointCost: number;
-    attackCountMax: number;
-    attackRange: number;
-    attackCommandPointCost: number;
-    attackDamage: number;
-    remainingAttacks: number;
-    attackMinRange: number;
-    destroyed: boolean;
-    hullTemplates: IHullTemplate[];
-    isFlagship: boolean;
-    isVisible: boolean;
-
+export class Ship extends ShipEntity {
     constructor(props: Readonly<IShip>) {
-        super();
-        Object.assign(this, props);
-        if (this.hulls) {
-            this.hulls = props.hulls.map((hull) => {
-                if (hull instanceof Hull) {
-                    return hull;
-                }
-                return new Hull(hull);
-            });
-        }
-    }
-
-    updateHull(hull: Partial<IHull>) {
-        if (!hull.id) return this;
-        const index = this.hulls.findIndex((h) => h.id === hull.id);
-
-        if (index === -1) return this;
-        const oldHull = this.hulls[index];
-        const updatedHull = new Hull({ ...oldHull, ...hull });
-        this.hulls[index] = updatedHull;
-        return this;
-    }
-
-    updateHulls(hulls: Partial<IHull>[]) {
-        hulls.forEach((hull) => {
-            this.updateHull(hull);
-        });
-        return this;
+        super(props);
     }
 
     getVisibleTiles() {
@@ -76,34 +25,6 @@ export class Ship extends Entity<Ship> implements IShip {
 
     removeInvisibleHullLocations() {
         this.hulls = this.hulls?.filter((h) => h.isVisible);
-        return this;
-    }
-
-    addHullLocations(hulls: IHull[]) {
-        if (!this.hulls) {
-            this.hulls = [];
-        }
-        hulls.forEach((h) => this.addHullLocation(h));
-        return this;
-    }
-
-    addHullLocation(hull: IHull) {
-        if (!(hull instanceof Hull)) {
-            this.hulls.push(new Hull(hull));
-            return this;
-        }
-        this.hulls.push(hull);
-        return this;
-    }
-
-    updateHullLocations(newHullLocations: Partial<IHull>[]) {
-        newHullLocations.forEach((newHull) => {
-            if (!newHull.id) return;
-            const index = this.hulls.findIndex((h) => h.id === newHull.id);
-            const oldHull = this.hulls[index];
-            const updatedHull = new Hull({ ...oldHull, ...newHull });
-            this.hulls[index] = updatedHull;
-        });
         return this;
     }
 
---
+++ shared/models/entities/ShipEntity.ts
@@ -0,0 +1,87 @@
+import { IHull, IHullTemplate, IShip } from "../../types";
+import { Hull } from "../Hull";
+import { Entity } from "./Entity";
+
+export class ShipEntity extends Entity<ShipEntity> implements IShip {
+    id: string;
+    playerId: string;
+    refNo: string;
+    name: string;
+    hullIds?: string[];
+    hulls?: Hull[];
+    dimensions: [number, number];
+    deployed: boolean;
+    commandPointCost: number;
+    movementRange: number;
+    remainingMovement: number;
+    movementCommandPointCost: number;
+    attackCountMax: number;
+    attackRange: number;
+    attackCommandPointCost: number;
+    attackDamage: number;
+    remainingAttacks: number;
+    attackMinRange: number;
+    destroyed: boolean;
+    hullTemplates: IHullTemplate[];
+    isFlagship: boolean;
+    isVisible: boolean;
+
+    constructor(props: Readonly<IShip>) {
+        super();
+        Object.assign(this, props);
+        if (this.hulls) {
+            this.hulls = props.hulls.map((hull) => {
+                if (hull instanceof Hull) {
+                    return hull;
+                }
+                return new Hull(hull);
+            });
+        }
+    }
+
+    public updateHull(hull: Partial<IHull>) {
+        if (!hull.id) return this;
+        const index = this.hulls.findIndex((h) => h.id === hull.id);
+
+        if (index === -1) return this;
+        const oldHull = this.hulls[index];
+        const updatedHull = new Hull({ ...oldHull, ...hull });
+        this.hulls[index] = updatedHull;
+        return this;
+    }
+
+    public updateHulls(hulls: Partial<IHull>[]) {
+        hulls.forEach((hull) => {
+            this.updateHull(hull);
+        });
+        return this;
+    }
+
+    public addHullLocations(hulls: IHull[]) {
+        if (!this.hulls) {
+            this.hulls = [];
+        }
+        hulls.forEach((h) => this.addHullLocation(h));
+        return this;
+    }
+
+    public addHullLocation(hull: IHull) {
+        if (!(hull instanceof Hull)) {
+            this.hulls.push(new Hull(hull));
+            return this;
+        }
+        this.hulls.push(hull);
+        return this;
+    }
+
+    public updateHullLocations(newHullLocations: Partial<IHull>[]) {
+        newHullLocations.forEach((newHull) => {
+            if (!newHull.id) return;
+            const index = this.hulls.findIndex((h) => h.id === newHull.id);
+            const oldHull = this.hulls[index];
+            const updatedHull = new Hull({ ...oldHull, ...newHull });
+            this.hulls[index] = updatedHull;
+        });
+        return this;
+    }
+}
