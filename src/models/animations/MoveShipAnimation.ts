14e240b68f0dc9a3fc3cb66ea9ef59f67f5f2fa4
FlixonStudios <chiakaiming@gmail.com>
2026-04-19 13:27:37 +0800

    extract out path segment logic

--- src/models/animations/MoveShipAnimation.ts
+++ src/models/animations/MoveShipAnimation.ts
@@ -1,14 +1,8 @@
 import { GAME_BOARD_ID } from "@shared/constants";
-import { ICellLoc } from "@shared/index";
+import { SegmentBuilder } from "@shared/utils/segment-builder";
 import { IMoveShipAnimationProps } from "../../types";
 import { BaseAnimation } from "./Animation";
 import { PivotWrapper } from "./PivotWrapper";
-
-interface ISegment {
-    pivotCell: ICellLoc;
-    rotateDegrees: number;
-    targetCell: ICellLoc;
-}
 
 export class MoveShipAnimation extends BaseAnimation {
     constructor(protected props: IMoveShipAnimationProps) {
@@ -28,9 +22,11 @@ export class MoveShipAnimation extends BaseAnimation {
         ) as HTMLElement[];
         const ship = this.animationLayer.wrapAndCopyToLayer(this.id, hullElements, shipId);
 
-        const startCenter = centerOfCells(hullLocations.map((h) => h.oldLoc));
-        const endCenter = centerOfCells(hullLocations.map((h) => h.newLoc));
-        const segments = this.buildSegments(startCenter, endCenter, startingOrientation);
+        const segmentBuilder = new SegmentBuilder();
+
+        const startCenter = segmentBuilder.centerOfCells(hullLocations.map((h) => h.oldLoc));
+        const endCenter = segmentBuilder.centerOfCells(hullLocations.map((h) => h.newLoc));
+        const segments = segmentBuilder.buildSegments(startCenter, endCenter, startingOrientation);
 
         let shipCell = startCenter;
 
@@ -44,56 +40,10 @@ export class MoveShipAnimation extends BaseAnimation {
                 duration: this.duration,
             });
             await pivot.rotate(segment.rotateDegrees);
-            await pivot.moveBy(delta(shipCell, segment.targetCell));
+            await pivot.moveBy(segmentBuilder.delta(shipCell, segment.targetCell));
             shipCell = pivot.release().cell;
         }
 
         this.animationLayer.destroyCopiedElements(this.id);
     }
-
-    private buildSegments(start: ICellLoc, end: ICellLoc, startingOrientation: number): ISegment[] {
-        const segments: ISegment[] = [];
-        const shipIsVertical = startingOrientation % 180 === 0;
-        const turn = turningPoint(start, end, shipIsVertical);
-
-        const rot1 = rotationToFace(start, turn, startingOrientation);
-        segments.push({ pivotCell: start, rotateDegrees: rot1, targetCell: turn });
-
-        if (!sameCell(turn, end)) {
-            const rot2 = rotationToFace(turn, end, startingOrientation + rot1);
-            segments.push({ pivotCell: turn, rotateDegrees: rot2, targetCell: end });
-        }
-
-        return segments.filter((s) => s.rotateDegrees !== 0 || !sameCell(s.pivotCell, s.targetCell));
-    }
-}
-
-function centerOfCells(cells: ICellLoc[]): ICellLoc {
-    const sumX = cells.reduce((a, c) => a + c[0], 0);
-    const sumY = cells.reduce((a, c) => a + c[1], 0);
-    return [sumX / cells.length, sumY / cells.length];
-}
-
-function turningPoint(start: ICellLoc, end: ICellLoc, isVertical: boolean): ICellLoc {
-    return isVertical ? [start[0], end[1]] : [end[0], start[1]];
-}
-
-function rotationToFace(from: ICellLoc, to: ICellLoc, currentOrientation: number): number {
-    if (sameCell(from, to)) return 0;
-    const dx = to[0] - from[0];
-    const dy = to[1] - from[1];
-    const target = Math.atan2(dy, dx) * (180 / Math.PI) + 90;
-    let rotation = target - currentOrientation;
-    // Normalize rotation to the range [-180, 180]
-    if (rotation > 180) rotation -= 360;
-    else if (rotation < -180) rotation += 360;
-    return rotation;
-}
-
-function delta(from: ICellLoc, to: ICellLoc): ICellLoc {
-    return [to[0] - from[0], to[1] - from[1]];
-}
-
-function sameCell(a: ICellLoc, b: ICellLoc): boolean {
-    return a[0] === b[0] && a[1] === b[1];
 }
