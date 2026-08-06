95ab8cad0571993e432d9e040ebf4c1a9e1d3c66
FlixonStudios <chiakaiming@gmail.com>
2026-08-06 19:57:21 +0800

    add base indexedDB

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,21 @@ import { App } from "./models/App";
 import { FEGameStateManager } from "./models/FEGameStateManager";
 import { GameManager } from "./models/GameManager";
 import { InteractionManager } from "./models/interaction-manager/InteractionManager";
+import { FrontendDB } from "./db";
+import { JwtHelper } from "../shared/auth/jwt-helper";
+
+export let dbObjectStore: IDBObjectStore;
+export let idb: FrontendDB;
+
+new FrontendDB()
+    .start(async (db) => {
+        const jwtHelper = new JwtHelper();
+        const { privateKey, publicKey } = await jwtHelper.generateKeyPair();
+
+        await db.store(privateKey, "privateKey");
+        await db.store(publicKey, "publicKey");
+    })
+    .then((db) => (idb = db));
 
 export const interactionManager = new InteractionManager();
 export const gameManager = new GameManager(); // effectively the DB
